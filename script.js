e0c9e87c2d771cb44e043e9fa69f659ca8ae3f98
Muktar Ali <muktarali1004@gmail.com>
2025-08-05 23:06:30 -0400

    Added delete and Read buttons, styled background with gradient

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const author = document.getElementById("author")
 const title = document.getElementById("title")
 const pages = document.getElementById("pages")
 const read = document.getElementById("read")
+const container = document.querySelector(".card-container");
+const content = document.createElement("div");
+const deleteButton = document.createElement("button");
+const seenButton = document.createElement("button");
+
+seenButton.textContent = "Have Read";
+deleteButton.textContent = "Delete";
+deleteButton.classList.add("delete_button");
+deleteButton.style.marginTop = "10px";
+
 
 const form = document.getElementById("myForm")
 form.style.display="none";
@@ -23,11 +33,34 @@ function addBooktoLibrary(title,author,pages,read){
 
 }
 addBooktoLibrary("A","joe",25,true)
-addBooktoLibrary("B","smith",25,false)
-addBooktoLibrary("C","matt",75,false)
-addBooktoLibrary("D","yo",25,true)
 
-myLibrary.forEach(Book => console.log(Book));
+
+
+
+myLibrary.forEach(Book => {
+    const container = document.querySelector(".card-container");
+    const content = document.createElement("div");
+    content.classList.add("card")
+    content.textContent = "Title:" + Book.title + "\n" + " Author: " + Book.author + "\n" + Book.pages + " pages "+  "\n" +  "Read: " + Book.read + "\n" ;
+    content.style.whiteSpace = "pre-line";
+    const deleteButton = document.createElement("button");
+    const seenButton = document.createElement("button");
+    seenButton.textContent = "Have Read";
+    deleteButton.textContent = "Delete";
+    content.appendChild(deleteButton);
+    content.appendChild(seenButton);
+    container.appendChild(content);
+    deleteButton.addEventListener("click", () => {
+    content.remove();
+    });
+    seenButton.addEventListener("click", () => {
+    Book.read = "True";
+    content.textContent = "Title:" + Book.title + "\n" + " Author: " + Book.author + "\n" + Book.pages + " pages "+  "\n" +  "Read: " + Book.read + "\n" ;
+
+  });
+
+
+});
 
 addButton.addEventListener('click', function() {
     form.style.display = 'flex'; 
@@ -36,13 +69,42 @@ addButton.addEventListener('click', function() {
 function stopDefault(event){
     event.preventDefault();
     addBooktoLibrary(author.value,title.value,pages.value,read.value);
-    console.log(myLibrary);
+    addCard();
+
+    
 } 
 
 submitButton.addEventListener('click', stopDefault, false);
 
+function addCard(){
+    const container = document.querySelector(".card-container");
+    const content = document.createElement("div");
+    content.classList.add("card")
+    content.textContent = "Title:" + title.value + "\n" + " Author: " + author.value+ "\n" + pages.value + " pages "+  "\n" +  "Read: " + read.value + "\n";
+    content.style.whiteSpace = "pre-line";
+    const deleteButton = document.createElement("button");
+    const seenButton = document.createElement("button");
+    seenButton.textContent = "Have Read";
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete_button");
+    content.appendChild(deleteButton);
+    content.appendChild(seenButton);
+    container.appendChild(content);
+    deleteButton.addEventListener("click", () => {
+    content.remove();
+      });
+    seenButton.addEventListener("click", () => {
+    read.value = "True";
+    content.textContent = "Title:" + title.value + "\n" + " Author: " + author.value+ "\n" + pages.value + " pages "+  "\n" +  "Read: " + read.value + "\n";
 
-    
+
+  });
+}
+
+
+
+
+
 
 
    
@@ -58,10 +120,3 @@ submitButton.addEventListener('click', stopDefault, false);
 
 
 
-
-
-
- /*this.info = function(){
-        return title + "by " + author +", " + pages + " pages " + read_status;
-    }
-    */

261fe2d31d4b340cbbd8343a55199fd345de2f73
Muktar Ali <muktarali1004@gmail.com>
2025-08-05 20:04:24 -0400

    Added form and newBook button

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const myLibrary = [];
+const addButton = document.getElementById("addButton");
+const submitButton = document.getElementById("submit-btn");
+const author = document.getElementById("author")
+const title = document.getElementById("title")
+const pages = document.getElementById("pages")
+const read = document.getElementById("read")
+
+const form = document.getElementById("myForm")
+form.style.display="none";
 
 function Book(title,author,pages,read){
     this.title = title;
@@ -17,9 +26,27 @@ addBooktoLibrary("A","joe",25,true)
 addBooktoLibrary("B","smith",25,false)
 addBooktoLibrary("C","matt",75,false)
 addBooktoLibrary("D","yo",25,true)
-addBooktoLibrary("E","dude",50,false)
 
 myLibrary.forEach(Book => console.log(Book));
+
+addButton.addEventListener('click', function() {
+    form.style.display = 'flex'; 
+    });
+
+function stopDefault(event){
+    event.preventDefault();
+    addBooktoLibrary(author.value,title.value,pages.value,read.value);
+    console.log(myLibrary);
+} 
+
+submitButton.addEventListener('click', stopDefault, false);
+
+
+    
+
+
+   
+
 
 
 
